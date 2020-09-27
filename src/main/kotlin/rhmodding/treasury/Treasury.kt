1c4253028e405cb511243a799174d920858e9841
chrislo27 <kinchrislo@gmail.com>
2020-09-27 13:21:32 -0700

    Bump version to v2.0.0

--- src/main/kotlin/rhmodding/treasury/Treasury.kt
+++ src/main/kotlin/rhmodding/treasury/Treasury.kt
@@ -20,7 +20,7 @@ import kotlin.system.exitProcess
 
 class Treasury : Application() {
     companion object {
-        val VERSION: Version = Version(2, 0, 0, "DEVELOPMENT")
+        val VERSION: Version = Version(2, 0, 0, "")
         val LOGGER: Logger = Logger.getLogger("Treasury").apply {
             level = Level.FINE
         }
